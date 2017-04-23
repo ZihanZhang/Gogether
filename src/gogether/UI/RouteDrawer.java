7c9bc60b971dc5f8365afb98a77d8043563f8bd6
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-04-23 16:24:38 -0400

    multi-particle

--- src/gogether/UI/RouteDrawer.java
+++ src/gogether/UI/RouteDrawer.java
@@ -15,6 +15,8 @@ import gogether.Logic.NoPassArea;
 public class RouteDrawer extends TimerTask{
 	Member m1 = new Member();
 	Member m2 = new Member();
+	Member m3 = new Member();
+	Member m4 = new Member();
 	
 	MapCanvas mapCanvas;
 	
@@ -29,6 +31,16 @@ public class RouteDrawer extends TimerTask{
 		m2.cury = 50;
 		m2.ca = mapCanvas.ca;
 		m2.npa = mapCanvas.npa;
+		
+		m3.curx = 80;
+		m3.cury = 50;
+		m3.ca = mapCanvas.ca;
+		m3.npa = mapCanvas.npa;
+		
+		m4.curx = 80;
+		m4.cury = 50;
+		m4.fa = mapCanvas.fa;
+		m4.npa = mapCanvas.npa;
 	}
 	
 	public void draw() {
@@ -76,6 +88,46 @@ public class RouteDrawer extends TimerTask{
 //		g2d.drawRect(m2.curx - 50, m2.cury - 50, 50, 50);
 //		System.out.println(m2.curx + " " + m2.cury);
 		m2.moveToNewLocation();
+		
+		m3.w = 1;
+		m3.c1 = 1;
+		m3.c2 = 1;
+		m3.gbestx = 700;
+		m3.gbesty = 370;
+		m3.oldx = m3.curx;
+		m3.oldy = m3.cury;
+		m3.setPbestxCom();
+		m3.setPbestyCom();
+		m3.setGbestx();
+		m3.setGbesty();
+		m3.setNextLocation();
+		m3.setNewLocation();
+		m3.avoidNoPassArea();
+//		System.out.println(mapCanvas.fa.leftupx + " " + mapCanvas.fa.leftupy + " " + mapCanvas.fa.rightdownx + " " + mapCanvas.fa.rightdowny);
+		g2d.drawLine(m3.curx, m3.cury, m3.newx, m3.newy);
+//		g2d.drawRect(m2.curx - 50, m2.cury - 50, 50, 50);
+//		System.out.println(m2.curx + " " + m2.cury);
+		m3.moveToNewLocation();
+		
+		m4.w = 1;
+		m4.c1 = 1;
+		m4.c2 = 1;
+		m4.gbestx = 700;
+		m4.gbesty = 370;
+		m4.oldx = m4.curx;
+		m4.oldy = m4.cury;
+		m4.setPbestxFast();
+		m4.setPbestyFast();
+		m4.setGbestx();
+		m4.setGbesty();
+		m4.setNextLocation();
+		m4.setNewLocation();
+		m4.avoidNoPassArea();
+//		System.out.println(npa.leftupx + " " + npa.leftupy + " " + npa.rightdownx + " " + npa.rightdowny);
+		g2d.drawLine(m4.curx, m4.cury, m4.newx, m4.newy);
+//		g2d.drawRect(m1.newx - 50, m1.newy - 50, 50, 50);
+//		System.out.println(m1.curx + " " + m1.cury);
+		m4.moveToNewLocation();
 		
 		mapCanvas.validate();
 	}
