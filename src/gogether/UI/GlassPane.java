5876fecdac56315385c750d0e546b3f7a83c0236
ZihanZhang <zhang.ziha@husky.neu.edu>
2017-04-27 10:30:17 -0400

    UI improvement

--- src/gogether/UI/GlassPane.java
+++ src/gogether/UI/GlassPane.java
@@ -3,6 +3,7 @@ package gogether.UI;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
@@ -38,18 +39,30 @@ public class GlassPane extends JComponent implements MouseListener{
 	
 	@Override
     protected void paintComponent(Graphics g) {
-//		for (FastArea fa: mc.fas) {
-//			if (fa.isInFastArea(x, y)) {
-//				g.setColor(Color.black);
-//				g.fillRect(fa.leftupx, fa.leftupy, (fa.rightdownx - fa.leftupx), (fa.rightdowny - fa.leftupy));
-//			}
-//		}
-//		for (ComfortArea ca: mc.cas) {
-//			if (ca.isInComArea(x, y)) {
-//				g.setColor(Color.black);
-//				g.fillRect(ca.leftupx, ca.leftupy, (ca.rightdownx - ca.leftupx), (ca.rightdowny - ca.leftupy));
-//			}
-//		}
+		for (FastArea fa: mc.fas) {
+			if (fa.isInFastArea(x, y)) {
+				int alpha = 127;
+				Color tYellow = new Color(255, 255, 102, alpha);
+				g.setColor(tYellow);
+				g.fillRoundRect(fa.leftupx, fa.leftupy, (fa.rightdownx - fa.leftupx), (fa.rightdowny - fa.leftupy), 15, 15);
+				g.setColor(Color.black);
+				g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+				g.drawString("Fast Area", (fa.leftupx + fa.rightdownx) / 2, (fa.leftupy + fa.rightdowny) / 2);
+				return;
+			}
+		}
+		for (ComfortArea ca: mc.cas) {
+			if (ca.isInComArea(x, y)) {
+				int alpha = 127;
+				Color tBlue = new Color(135,206,250, alpha);
+				g.setColor(tBlue);
+				g.fillRoundRect(ca.leftupx, ca.leftupy, (ca.rightdownx - ca.leftupx), (ca.rightdowny - ca.leftupy), 15, 15);
+				g.setColor(Color.black);
+				g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+				g.drawString("Fast Area", (ca.leftupx + ca.rightdownx) / 2, (ca.leftupy + ca.rightdowny) / 2);
+				return;
+			}
+		}
 	}
 
 	@Override
@@ -71,7 +84,12 @@ public class GlassPane extends JComponent implements MouseListener{
 				timer.schedule(new RouteDrawer(mc, 1, mlc), 0L, 100L);
 			}
 		}
-		System.out.println(containerPoint.x + " " + containerPoint.y); 
+		else {
+			Point mapCanvasPoint = SwingUtilities.convertPoint(this, glassPanePoint, mc);
+			x = mapCanvasPoint.x;
+			y = mapCanvasPoint.y;
+			this.repaint();
+		}
 	}
 
 	@Override
